9dee646d319b4a9abb5923ba6f8327ce0069b3c4
dmikhr <dmitrypkh@gmail.com>
2019-10-30 09:39:31 +0300

    cleaning code 3

--- spec/services/page_downloader_spec.rb
+++ spec/services/page_downloader_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Services::PageDownloader do
   let(:website) { build(:website) }
-  vcr_options = { :record => :new_episodes }
 
   it 'downloads page' do
     expect(Services::PageDownloader).to receive(:call).with(website.url)
