80b1a76e4b4b7230b067a6be9d8a078596d67ef9
dmikhr <dmitrypkh@gmail.com>
2019-11-02 15:17:46 +0300

    pages controller and routes

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -15,9 +15,7 @@ class WebsitesController < ApplicationController
     end
   end
 
-  def show
-
-  end
+  def show; end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
-  
+
   root to: 'websites#new'
 
-  resources :websites, only: %i[new create show]
+  resources :websites, only: %i[new create show] do
+    resources :pages, only: %i[create show]
+  end
+
 end
