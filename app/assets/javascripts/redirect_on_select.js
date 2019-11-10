ef5d25d4c9c1e53734386bfbb2b4848367f83e08
dmikhr <dmitrypkh@gmail.com>
2019-11-10 20:21:41 +0300

    code cleaning

--- app/assets/javascripts/redirect_on_select.js
+++ app/assets/javascripts/redirect_on_select.js
@@ -1,10 +1,3 @@
 function redirectToReport(value) {
   window.location = "/websites/" + value;
 }
-// $(document).ready
-// $(document).on('change', '.select_report_version', function(){
-//     var reportId = $(this).val();
-//     alert (reportId);
-//     $.get('/websites/reportId');
-//
-// });
