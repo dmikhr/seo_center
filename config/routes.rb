2c84eb19a342c16bfce6d3eba9740e6d4fe74966
dmikhr <dmitrypkh@gmail.com>
2019-11-12 16:03:53 +0300

    only admin can access Sidekiq interface

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   devise_for :users
-  mount Sidekiq::Web => '/sidekiq'
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   root to: 'websites#new'
 
